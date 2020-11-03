f01bb18bd158967e935f648fb26b6413a27258b9
moh151 <67608414+moh151@users.noreply.github.com>
2020-11-04 00:53:22 +0200

    updated the jscode to store data locally

--- script.js
+++ script.js
@@ -1,75 +1,70 @@
-const container=document.querySelector(".container");
-const seats=document.querySelectorAll("row .seat:not(.occupied");
-const count=document.getElementById("count");
-const movieSelect=document.getElementById("movie");
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
 
 populateUI();
 
-let ticketPrice=+movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
-
-//save selcted movie index and price
-function setMovieData(movieIndex,moviePrice){
-localStorage.setItem("selctedMovieIndex",movieIndex);
-localStorage.setItem("selectedMoviePrice",moviePrice);
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
-//update total and count
-function updateSelectedCount(){
-const selectedSeats=document.querySelectorAll(".row .seat.selected");
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-const seatsIndex=[...selectedSeats].map((seat)=> [...seats].indexOf(seat)
-);
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
-localStorage.setItem("selectedSeats",JSON.stringify(seatsIndex))
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
-const selectedSeatsCount=selectedSeats.length;
+  const selectedSeatsCount = selectedSeats.length;
 
-count.innerText=selectedSeatsCount;
-total.innerText=selectedSeatsCount*ticketPrice;
-
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-// get data from local Storage and populate UI
-function populateUI(){
-     const selectedSeats=JSON.parse(localStorage.getItem("selctedSeats"));
-     if(selectedSeats !==null && selectedSeats.length>0){
-         seats.forEach((seat,index)=>{
-             if(selectedSeats.indexOf(index)>-1){
-        seat.classList.add("selected");  
-             }
-         });
-     }
+// Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
 
-      const selctedMovieIndex=localStorage.getItem("selectedMovieIndex");
-        
-      if(selctedMovieIndex !==null){
-          movieSelect.selectedIndex=selctedMovieIndex;
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
       }
+    });
+  }
 
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
 }
 
-//movie select event
-movieSelect.addEventListener("change",e=>{
-ticketPrice=e.target.value;
-
-setMovieData(e.target.selectedIndex,e.target.value);
-updateSelectedCount(); 
-})
-
-//seat click event
-container.addEventListener("click",e=>{
-    
-if(
-e.target.classList.contains("seat") && 
-!e.target.classList.contains("occupied")
-){
-e.target.classList.toggle("selected"); 
-updateSelectedCount();        
-}
-
-
+// Movie select event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
 });
 
-//intial count and total set
+// Seat click event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
+
+// Initial count and total set
 updateSelectedCount();
